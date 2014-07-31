b6dc9576e3c56830e3a5344bcdec61deea739880
sun <sun@unleashedmind.com>
2014-07-31 23:12:01 +0200

    [FIXME] drupal_rewrite_settings() does not support stream wrappers.

--- core/modules/system/src/Tests/System/SettingsRewriteTest.php
+++ core/modules/system/src/Tests/System/SettingsRewriteTest.php
@@ -100,9 +100,9 @@ EXPECTED
     );
     foreach ($tests as $test) {
       $filename = Settings::get('file_public_path', conf_path() . '/files') . '/mock_settings.php';
-      file_put_contents(DRUPAL_ROOT . '/' . $filename, "<?php\n" . $test['original'] . "\n");
+      file_put_contents($filename, "<?php\n" . $test['original'] . "\n");
       drupal_rewrite_settings($test['settings'], $filename);
-      $this->assertEqual(file_get_contents(DRUPAL_ROOT . '/' . $filename), "<?php\n" . $test['expected'] . "\n");
+      $this->assertEqual(file_get_contents($filename), "<?php\n" . $test['expected'] . "\n");
     }
 
     // Test that <?php gets added to the start of an empty settings file.
@@ -118,12 +118,12 @@ EXPECTED
     );
     // Make an empty file.
     $filename = Settings::get('file_public_path', conf_path() . '/files') . '/mock_settings.php';
-    file_put_contents(DRUPAL_ROOT . '/' . $filename, "");
+    file_put_contents($filename, "");
 
     // Write the setting to the file.
     drupal_rewrite_settings($test['settings'], $filename);
 
     // Check that the result is just the php opening tag and the settings.
-    $this->assertEqual(file_get_contents(DRUPAL_ROOT . '/' . $filename), "<?php\n" . $test['expected'] . "\n");
+    $this->assertEqual(file_get_contents($filename), "<?php\n" . $test['expected'] . "\n");
   }
 }
